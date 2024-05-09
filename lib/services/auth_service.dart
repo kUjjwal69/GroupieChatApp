d86c0aef3157ffbaf5345fe8f7f16cac55fb7282
Ujjwal kumar <kumarujjwal4200@gmail.com>
2024-05-10 01:17:10 +0530

    Initial commit

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,53 @@
+
+// ignore_for_file: depend_on_referenced_packages, unused_local_variable, unnecessary_null_comparison
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_1/helper/helper_function.dart';
+import 'package:flutter_application_1/services/database_service.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // login
+  Future loginWithUserNameandPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      return true;
+        } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // register
+  Future registerUserWithEmailandPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        // call our database service to update the user data.
+        await DatabaseService(uid: user.uid).savingUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // signout
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
+}
